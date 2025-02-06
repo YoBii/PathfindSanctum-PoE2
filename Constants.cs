b0521fd916f322158a4c7232b84f608b63e175e2
YoBii <xschoppmeier+github@gmail.com>
2025-02-06 23:53:02 +0100

    add constants

---
+++ Constants.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathfindSanctum {
+    internal static class Constants {
+        internal static class Profiles {
+            public const string Default = "Default";
+            public const string NoHit = "No-Hit";
+        }
+
+        internal static class RoomTypes {
+            public const string Gauntlet = "Gauntlet";
+            public const string Hourglass = "Hourglass";
+            public const string Chalice = "Chalice";
+            public const string Ritual = "Ritual";
+            public const string Escape = "Escape";
+            public const string Boss = "Boss";
+        }
+
+        internal static class AfflictionTypes {
+            public const string GlassShard = "Glass Shard";
+            public const string GhastlyScythe = "Ghastly Scythe";
+            public const string VeiledSight = "Veiled Sight";
+            public const string MyriadAspersions = "Myriad Aspersions";
+            public const string DeceptiveMirror = "Deceptive Mirror";
+            public const string PurpleSmoke = "Purple Smoke";
+            public const string GoldenSmoke = "Golden Smoke";
+            public const string RedSmoke = "Red Smoke";
+            public const string BlackSmoke = "Black Smoke";
+            public const string RapidQuicksand = "Rapid Quicksand";
+            public const string DeadlySnare = "Deadly Snare";
+            public const string ForgottenTraditions = "Forgotten Traditions";
+            public const string SeasonOfFamine = "Season of Famine";
+            public const string OrbOfNegation = "Orb of Negation";
+            public const string WinterDrought = "Winter Drought";
+            public const string BrandedBalbalakh = "Branded Balbalakh";
+            public const string ChiselledStone = "Chiselled Stone";
+            public const string WeakenedFlesh = "Weakened Flesh";
+            public const string Untouchable = "Untouchable";
+            public const string CostlyAid = "Costly Aid";
+            public const string BluntSword = "Blunt Sword";
+            public const string SpikedShell = "Spiked Shell";
+            public const string SuspectedSympathiser = "Suspected Sympathiser";
+            public const string Haemorrhage = "Haemorrhage";
+            public const string CorrosiveConcoction = "Corrosive Concoction";
+            public const string IronManacles = "Iron Manacles";
+            public const string ShatteredShield = "Shattered Shield";
+            public const string UnquenchedThirst = "Unquenched Thirst";
+            public const string UnassumingBrick = "Unassuming Brick";
+            public const string TraditionsDemand = "Tradition's Demand";
+            public const string FiendishWings = "Fiendish Wings";
+            public const string HungryFangs = "Hungry Fangs";
+            public const string WornSandals = "Worn Sandals";
+            public const string TradeTariff = "Trade Tariff";
+            public const string DeathToll = "Death Toll";
+            public const string SpikedExit = "Spiked Exit";
+            public const string ExhaustedWells = "Exhausted Wells";
+            public const string GateToll = "Gate Toll";
+            public const string LeakingWaterskin = "Leaking Waterskin";
+            public const string LowRivers = "Low Rivers";
+            public const string SharpenedArrowhead = "Sharpened Arrowhead";
+            public const string RustedMallet = "Rusted Mallet";
+            public const string ChainsOfBinding = "Chains of Binding";
+            public const string DishonouredTattoo = "Dishonoured Tattoo";
+            public const string TatteredBlindfold = "Tattered Blindfold";
+            public const string DarkPit = "Dark Pit";
+            public const string HonedClaws = "Honed Claws";
+        }
+
+        internal static class RewardTypes {
+            public const string GoldKey = "Gold Key";
+            public const string SilverKey = "Silver Key";
+            public const string BronzeKey = "Bronze Key";
+            public const string GoldenCache = "Golden Cache";
+            public const string SilverCache = "Silver Cache";
+            public const string BronzeCache = "Bronze Cache";
+            public const string LargeFountain = "Large Fountain";
+            public const string Fountain = "Fountain";
+            public const string PledgeToKochai = "Pledge to Kochai";
+            public const string HonourHalani = "Honour Halani";
+            public const string HonourAhkeli = "Honour Ahkeli";
+            public const string HonourOrbala = "Honour Orbala";
+            public const string HonourGalai = "Honour Galai";
+            public const string HonourTabana = "Honour Tabana";
+            public const string Merchant = "Merchant";
+        }
+    }
+
+}
